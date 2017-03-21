fd222e0edda083e0e6fc40d764e1ebcd45c434c5
Eric Larson <ewlarson@umn.edu>
2017-03-21 15:58:07 -0500

    Solr updates
    Added id field to schema.xml; search results working, non-working show
    pages.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -38,8 +38,8 @@ class CatalogController < ApplicationController
     #}
 
     # solr field configuration for search results/index views
-    config.index.title_field = 'title_display'
-    config.index.display_type_field = 'format'
+    config.index.title_field = 'pubTitleDisplay'
+    config.index.display_type_field = 'docTypeDescription'
     #config.index.thumbnail_field = 'thumbnail_path_ss'
 
     # solr field configuration for document/show views
@@ -71,21 +71,23 @@ class CatalogController < ApplicationController
     #  (useful when user clicks "more" on a large facet and wants to navigate alphabetically across a large set of results)
     # :index_range can be an array or range of prefixes that will be used to create the navigation (note: It is case sensitive when searching values)
 
-    config.add_facet_field 'format', label: 'Format'
-    config.add_facet_field 'pub_date', label: 'Publication Year', single: true
-    config.add_facet_field 'subject_topic_facet', label: 'Topic', limit: 20, index_range: 'A'..'Z'
-    config.add_facet_field 'language_facet', label: 'Language', limit: true
-    config.add_facet_field 'lc_1letter_facet', label: 'Call Number'
-    config.add_facet_field 'subject_geo_facet', label: 'Region'
-    config.add_facet_field 'subject_era_facet', label: 'Era'
+    config.add_facet_field 'docTypeDescription', label: 'Format'
+    config.add_facet_field 'pubDate', label: 'Publication Date', single: true
+    config.add_facet_field 'authors', label: 'Author', limit: 20, index_range: 'A'..'Z'
+    config.add_facet_field 'pubName', label: 'Publication Name', limit: 20, index_range: 'A'..'Z'
+    config.add_facet_field 'affiliateAuthorsDeptName', label: 'Department', limit: true
 
-    config.add_facet_field 'example_pivot_field', label: 'Pivot Field', :pivot => ['format', 'language_facet']
+    # config.add_facet_field 'lc_1letter_facet', label: 'Call Number'
+    # config.add_facet_field 'subject_geo_facet', label: 'Region'
+    # config.add_facet_field 'subject_era_facet', label: 'Era'
 
-    config.add_facet_field 'example_query_facet_field', label: 'Publish Date', :query => {
-       :years_5 => { label: 'within 5 Years', fq: "pub_date:[#{Time.zone.now.year - 5 } TO *]" },
-       :years_10 => { label: 'within 10 Years', fq: "pub_date:[#{Time.zone.now.year - 10 } TO *]" },
-       :years_25 => { label: 'within 25 Years', fq: "pub_date:[#{Time.zone.now.year - 25 } TO *]" }
-    }
+    # config.add_facet_field 'example_pivot_field', label: 'Pivot Field', :pivot => ['format', 'language_facet']
+
+    # config.add_facet_field 'example_query_facet_field', label: 'Publish Date', :query => {
+    #   :years_5 => { label: 'within 5 Years', fq: "pub_date:[#{Time.zone.now.year - 5 } TO *]" },
+    #   :years_10 => { label: 'within 10 Years', fq: "pub_date:[#{Time.zone.now.year - 10 } TO *]" },
+    #   :years_25 => { label: 'within 25 Years', fq: "pub_date:[#{Time.zone.now.year - 25 } TO *]" }
+    # }
 
 
     # Have BL send all facet field names to Solr, which has been the default
@@ -95,32 +97,30 @@ class CatalogController < ApplicationController
 
     # solr fields to be displayed in the index (search results) view
     #   The ordering of the field names is the order of the display
-    config.add_index_field 'title_display', label: 'Title'
-    config.add_index_field 'title_vern_display', label: 'Title'
-    config.add_index_field 'author_display', label: 'Author'
-    config.add_index_field 'author_vern_display', label: 'Author'
-    config.add_index_field 'format', label: 'Format'
-    config.add_index_field 'language_facet', label: 'Language'
-    config.add_index_field 'published_display', label: 'Published'
-    config.add_index_field 'published_vern_display', label: 'Published'
-    config.add_index_field 'lc_callnum_display', label: 'Call number'
+    config.add_index_field 'pubTitleDisplay', label: 'Title'
+    config.add_index_field 'authors', label: 'Author'
+    config.add_index_field 'docTypeDescription', label: 'Format'
+    config.add_index_field 'pubDate', label: 'Publication Date'
+    config.add_index_field 'pubName', label: 'Publication Name'
+    # config.add_index_field 'published_vern_display', label: 'Published'
+    # config.add_index_field 'lc_callnum_display', label: 'Call number'
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
-    config.add_show_field 'title_display', label: 'Title'
-    config.add_show_field 'title_vern_display', label: 'Title'
-    config.add_show_field 'subtitle_display', label: 'Subtitle'
-    config.add_show_field 'subtitle_vern_display', label: 'Subtitle'
-    config.add_show_field 'author_display', label: 'Author'
-    config.add_show_field 'author_vern_display', label: 'Author'
-    config.add_show_field 'format', label: 'Format'
-    config.add_show_field 'url_fulltext_display', label: 'URL'
-    config.add_show_field 'url_suppl_display', label: 'More Information'
-    config.add_show_field 'language_facet', label: 'Language'
-    config.add_show_field 'published_display', label: 'Published'
-    config.add_show_field 'published_vern_display', label: 'Published'
-    config.add_show_field 'lc_callnum_display', label: 'Call number'
-    config.add_show_field 'isbn_t', label: 'ISBN'
+    config.add_show_field 'pubTitleDisplay', label: 'Title'
+    config.add_show_field 'authors', label: 'Author'
+    config.add_show_field 'docTypeDescription', label: 'Format'
+    config.add_show_field 'pubDate', label: 'Publication Date'
+    config.add_show_field 'pubName', label: 'Publication Name'
+    config.add_show_field 'volume', label: 'Volume'
+    config.add_show_field 'issue', label: 'Issue'
+    config.add_show_field 'pageRange', label: 'Pages'
+    config.add_show_field 'affiliateAuthorsDeptName', label: 'Departments'
+    config.add_show_field 'citedByCount', label: 'Cited By Count'
+    config.add_show_field 'pmid', label: 'PMID'
+    config.add_show_field 'doi', label: 'DOI'
+    config.add_show_field 'scopus_eid', label: 'Scopus E-ID'
+    config.add_show_field 'scopus_pubid', label: 'Scopus Pub-ID'
 
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
@@ -140,16 +140,16 @@ class CatalogController < ApplicationController
     # solr request handler? The one set in config[:default_solr_parameters][:qt],
     # since we aren't specifying it otherwise.
 
-    config.add_search_field 'all_fields', label: 'All Fields'
+    config.add_search_field 'generalSearch', label: 'All Fields'
 
 
     # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
     # of Solr search fields.
 
-    config.add_search_field('title') do |field|
+    config.add_search_field('pubTitleIndex') do |field|
       # solr_parameters hash are sent to Solr as ordinary url query params.
-      field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
+      field.solr_parameters = { :'spellcheck.dictionary' => 'pubTitleIndex' }
 
       # :solr_local_parameters will be sent using Solr LocalParams
       # syntax, as eg {! qf=$title_qf }. This is neccesary to use
@@ -161,8 +161,8 @@ class CatalogController < ApplicationController
       }
     end
 
-    config.add_search_field('author') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
+    config.add_search_field('authors') do |field|
+      field.solr_parameters = { :'spellcheck.dictionary' => 'authors' }
       field.solr_local_parameters = {
         qf: '$author_qf',
         pf: '$author_pf'
@@ -172,23 +172,23 @@ class CatalogController < ApplicationController
     # Specifying a :qt only to show it's possible, and so our internal automated
     # tests can test it. In this case it's the same as
     # config[:default_solr_parameters][:qt], so isn't actually neccesary.
-    config.add_search_field('subject') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
-      field.qt = 'search'
-      field.solr_local_parameters = {
-        qf: '$subject_qf',
-        pf: '$subject_pf'
-      }
-    end
+    # config.add_search_field('subject') do |field|
+    #  field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
+    #  field.qt = 'search'
+    #  field.solr_local_parameters = {
+    #    qf: '$subject_qf',
+    #    pf: '$subject_pf'
+    #  }
+    # end
 
     # "sort results by" select (pulldown)
     # label in pulldown is followed by the name of the SOLR field to sort by and
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
-    config.add_sort_field 'score desc, pub_date_sort desc, title_sort asc', label: 'relevance'
-    config.add_sort_field 'pub_date_sort desc, title_sort asc', label: 'year'
-    config.add_sort_field 'author_sort asc, title_sort asc', label: 'author'
-    config.add_sort_field 'title_sort asc, pub_date_sort desc', label: 'title'
+    config.add_sort_field 'score desc, pubDate desc, pubTitleIndex asc', label: 'relevance'
+    config.add_sort_field 'pubDate desc, pubTitleIndex asc', label: 'year'
+    config.add_sort_field 'authors asc, pubTitleIndex asc', label: 'author'
+    config.add_sort_field 'pubTitleIndex asc, pubDate desc', label: 'title'
 
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
